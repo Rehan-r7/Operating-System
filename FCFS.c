15cc225e779712bf88fcf0bd6a1db53f3a5fb4c1
Rehan khan <111696261+Rehan-r7@users.noreply.github.com>
2023-04-04 10:20:01 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-int main()
+#define MAX 20
+int fcfs(int A[],int c,int cur,int res,int size)
 {
-    int A[] = {34, 28, 111, 123, 148, 199};
-    int c = 200;  // Total cylinders
-    int cur = 53; // current head
-    int res = 0;  // variable to store result
-
     res = cur - A[0]; // current track - first track in the list
    
     printf("%d ", res);
     
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < size-1; i++)
     {
         if (A[i] > c) // break if given track is out of cylinders
             break;
@@ -23,5 +19,21 @@ int main()
 
         printf(" %d ", res);
     }
-    printf("\nThe Final Time Taken : %d", res);
+    return res;
+}
+int main()
+{
+    int A[MAX];
+    int size;
+    printf("Enter the size of the array : ");
+    scanf("%d",&size);
+    
+    for(int i = 0; i < size; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    
+    int ans = fcfs(A,200,53,0,size);// cylinder = 200,current = 53,result =0 
+   printf("\nThe Final Time Taken : %d", ans); 
+
 }
