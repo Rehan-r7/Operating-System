d77f118a9b34cdd685808f20b688d00441a1131d
Rehan khan <111696261+Rehan-r7@users.noreply.github.com>
2023-04-11 02:08:55 +0530

    Update scan.c

--- scan.c
+++ scan.c
@@ -1,88 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 20
-void sort(int A[],int n)
+#define MAX 50
+void sort(int A[], int n) // Insertion sort
 {
-    int i,j,temp;
-    for (  i = 1; i < n; i++)
+    int i, j, temp;
+    for (i = 1; i < n; i++)
     {
         temp = A[i];
-          j = i-1;
-        while (j>=0 && A[j]>temp)
+        j = i - 1;
+        while (j >= 0 && A[j] > temp)
         {
-           A[j+1] = A[j];
+            A[j + 1] = A[j];
             j--;
-        }   
-            A[j+1]=temp;       
-    }  
-}
-void display(int A[],int n)
-{
-    printf("printing the elements : ");
-    for (int  i = 0; i < n; i++)
-    {
-        printf("%d ",A[i]);
+        }
+        A[j + 1] = temp;
     }
 }
-int fcfs(int A[],int c,int cur,int res,int size,int dir)
+void display(int A[], int n) // for printing array
 {
+    printf("Sorted Array : ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+int scan(int A[], int c, int cur, int res, int size, int dir)
+{ // cylinders, current, result, size, direction
     int temp;
     int i;
-   // res = cur - A[0]; // current track - first track in the list
-   for( temp =0 ;temp<size;temp++)
-   {
-       if(A[temp]==cur)
-       {
-            break;
-       }
-   }
-    printf("%d ", A[temp]);
-    
-    if(dir==0)//right
+    for (temp = 0; temp < size; temp++) // gives the index of current head
     {
-        for( i = temp; i<size-1;i++)
+        if (A[temp] == cur)
         {
-            res+= A[i+1]-A[i];
-           
-            printf("%d - %d",A[i+1],A[i]);
-            //res+= abs((c-1) - res);
-            printf(" (%d) ",res);
+            break;
         }
-         printf( " %d ",A[i]);
-         res+=(c-1) - A[i];
+    }
+    if (dir == 0) // right
+    {
+        for (i = temp; i < size - 1; i++) // right sub-array
+        {
+            res += A[i + 1] - A[i]; // higher value - lower value
 
-         printf("%d - %d",(c-1),A[i]);
-         printf( " %d ",A[temp-1]);
-         res+=(c-1) - A[temp-1];
-         printf("%d - %d",(c-1),A[temp-1]);
-        for(int i = temp-1;i>0;i--)
-        {
-            res+= A[i]-A[i-1];
-             printf("%d - %d",A[i],A[i-1]);
-             printf(" (%d) ",res);
+            printf("%d - %d", A[i + 1], A[i]); // printing difference
+            printf(" (%d) ", res);             // result after each difference
         }
-    }   
-      return res;
+        res += (c - 1) - A[i];        // eg.:- (c-1) 200-1 = 199 i.e last cylinder
+                                      // last cylinder - last right element in the array
+        res += (c - 1) - A[temp - 1]; // last cylinder - element before current
+
+        for (i = temp - 1; i > 0; i--) // left sub-array
+        {
+            res += A[i] - A[i - 1];            // higher value - lower value
+            printf("%d - %d", A[i], A[i - 1]); // printing difference
+            printf(" (%d) ", res);
+        }
+    }
+    else if (dir == 1) // left
+    {
+        for (i = temp; i > 0; i--) // left sub-array
+        {
+            res += A[i] - A[i - 1];
+            printf("%d - %d", A[i], A[i - 1]);
+            printf(" (%d) ", res);
+        }
+
+        res += A[temp + 1] - A[i];            // element after current - first element in the list
+        for (i = temp + 1; i < size - 1; i++) // right sub-array
+        {
+            res += A[i + 1] - A[i];
+            printf("%d - %d", A[i + 1], A[i]);
+            printf(" (%d) ", res);
+        }
+        res += (c - 1) - A[i]; // last cylinder - last right element in the list
+    }
+    return res;
 }
 
 int main()
 {
     int A[MAX];
-    int size,dir;
+    int size, dir;
+    int head, cylinders;
     printf("Enter the size of the array : ");
-    scanf("%d",&size);
-    
-    for(int i = 0; i < size; i++)
+    scanf("%d", &size);
+
+    for (int i = 0; i < size; i++)
     {
-        scanf("%d",&A[i]);
+        scanf("%d", &A[i]);
     }
     printf("Enter direction 0 or 1 (Right = 0 and Left = 1) : ");
-    scanf("%d",&dir);
-    sort(A,size);
-    display(A,size);
-    printf("\n");
-    
-   int ans = fcfs(A,200,53,0,size,dir);// cylinder = 200,current = 53,result =0 
-   printf("\nThe Final Time Taken : %d", ans); 
+    scanf("%d", &dir);
 
+    if (dir == 0 || dir == 1)
+    {
+        sort(A, size); // calling sort function
+
+        display(A, size); // calling display fuction
+        printf("\n");
+
+        printf("Enter current head : ");
+        scanf("%d", &head);
+
+        printf("Enter total cylinders : ");
+        scanf("%d", &cylinders);
+        // res = 0 (initial value)
+        int ans = scan(A, cylinders, head, 0, size, dir); // storing result
+        printf("\nTotal Seek Time Taken : %d", ans);
+    }
+    else
+    {
+        printf("Please Enter valid directon!!!");
+    }
 }
