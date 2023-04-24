7c8ec7bd0473fbac5870a94909e895e807f576ae
Rehan khan <111696261+Rehan-r7@users.noreply.github.com>
2023-04-24 20:15:52 +0530

    Update process.c

--- process.c
+++ process.c
@@ -23,6 +23,31 @@ int main()
 		
 		
 	}
+	/* Alternate method
+	int A1[4][2];
+    int A2[4][3];
+    
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            scanf("%d",&A1[i][j]);
+        }
+    }
+    printf("Arrival BurstTime\n");
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<1;j++)
+        {
+            A2[i][j] = A1[i][j]+A1[i][j+1];
+            A2[i][j+1] = A2[i][j] - A1[i][j];
+            A2[i][j+2] = A2[i][j+1] - A1[i][j+1];
+            
+            printf("%d    ",&A2[i][j]);
+        }
+        printf("\n");
+    }
+    */
 	
 	
 	return 0;
